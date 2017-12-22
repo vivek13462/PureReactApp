66796a9cd13c43acab47bb32033ec952419b22d4
vivek.13462 <vivek.13462@gmail.com>
2017-12-21 22:09:26 -0800

    refactoring elements using jsx

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { createElement } = React
+
 const { render } = ReactDOM
-const title = createElement(
-'h1',
-    {id:'title', className: 'header'},
-    'Hello World'
-)
+
+
 
 render(
-title,
+<h1 id='title'
+    className='header'
+    style={{backgroundColor:'orange',color:'white',fontFamily:'verdana'}}>
+    Hello World
+    </h1>,
     document.getElementById('react-container')
 )
