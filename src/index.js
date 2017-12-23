56960cd35da1808a9709255358788ad315087650
vivek.13462 <vivek.13462@gmail.com>
2017-12-23 00:25:14 -0800

    composed components and displayed child component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayCount total={50}
-				 powder={20}
-				 backcountry={10}
-				 goal={100} />,
+	<SkiDayList days={
+    [
+    {
+    resort: "Carle Montes",
+    date: new Date("12/8/2017"),
+    powder: true,
+    backcountry: false
+    },
+        {
+    resort: "Belagio",
+    date: new Date("2/8/2017"),
+    powder: false,
+    backcountry: false
+    },
+        {
+    resort: "Venetian",
+    date: new Date("6/8/2017"),
+    powder: false,
+    backcountry: true
+    }
+    ]
+    }
+    />,
 	document.getElementById('react-container')
 )
