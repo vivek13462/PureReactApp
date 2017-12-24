05a31fe656dd216cbbe617213b85f0482e7857f9
vivek.13462 <vivek.13462@gmail.com>
2017-12-23 21:05:42 -0800

    added getInitialState function in App.js file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
+import './stylesheets/ui.scss'
+import { App } from './components/App'
 
 window.React = React
 
 render(
-	<SkiDayList days={
-    [
-    {
-    resort: "Carle Montes",
-    date: new Date("12/8/2017"),
-    powder: true,
-    backcountry: false
-    },
-        {
-    resort: "Belagio",
-    date: new Date("2/8/2017"),
-    powder: false,
-    backcountry: false
-    },
-        {
-    resort: "Venetian",
-    date: new Date("6/8/2017"),
-    powder: false,
-    backcountry: true
-    }
-    ]
-    }
-    />,
+	<App />,
 	document.getElementById('react-container')
 )
