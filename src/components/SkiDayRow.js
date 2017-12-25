7349e266d9af7204927c7fda3bd91d32d8e95a79
vivek.13462 <vivek.13462@gmail.com>
2017-12-24 20:59:53 -0800

    implemented two-way function binding

--- src/components/SkiDayRow.js
+++ src/components/SkiDayRow.js
@@ -3,20 +3,30 @@ import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
 import { PropTypes } from 'react'
 
-export const SkiDayRow = ({resort, date, powder, backcountry}) => (
-    
-    <tr>
-    <td>{date.getMonth()+1}/{date.getDate()}/{date.getFullYear()}</td>
-    <td>{resort}</td>
-    <td>{(powder) ? <SnowFlake /> : null}</td>
-    <td>{(backcountry) ? <Terrain /> : null}</td>
-    </tr>
+export const SkiDayRow = ({resort, date, 
+							powder, backcountry}) => (
+	<tr>
+		<td>
+			{date}
+		</td>
+		<td>
+			{resort}
+		</td>
+		<td>
+			{(powder) ? <SnowFlake/> : null}
+		</td>
+		<td>
+			{(backcountry) ? <Terrain /> : null}
+		</td>
+	</tr>						
 
 )
 
 SkiDayRow.propTypes = {
-  resort: PropTypes.string.isRequired,
-  date: PropTypes.instanceOf(Date).isRequired,
-  powder: PropTypes.bool,
-  backcountry: PropTypes.bool
+	resort: PropTypes.string.isRequired,
+	date: PropTypes.string.isRequired,
+	powder: PropTypes.bool,
+	backcountry: PropTypes.bool
 }
+
+
